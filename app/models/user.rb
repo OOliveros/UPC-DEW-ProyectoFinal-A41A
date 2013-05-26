deab11a15641092b56868b50bba9aca27fc733a8
Augusto Bravo <bravoaugusto300@gmail.com>
2013-05-25 20:25:42 -0500

    reverse

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-  attr_accessible :dateregister, :document, :email, :lastname, :name, :password, :secondlastname, :sex, :typeuser, :typedocument
-=======
   attr_accessible :dateregister, :document, :email, :lastname, :name, :password, :secondlastname, :sex, :typedocument, :typeuser
->>>>>>> fcdccdd71f4f6da71016d84dc7f00053d264b3ec
 end
