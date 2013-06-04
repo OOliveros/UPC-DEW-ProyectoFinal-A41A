57c38f10f9f4602e824dc1aface2926d0a5ad6bc
Augusto Bravo <bravoaugusto300@gmail.com>
2013-06-04 00:08:41 -0500

    compilado plantilla 1

---
+++ ConcertUPC1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConcertUPC1::Application.config.secret_token = 'a874b63a7a73aa810779ea85a723120a96834a20dc0a04db1502a59d4a50ce95ed9e91ad27066bbcbbde9497c6ce0460ff183462013ac645db498cbc7af8977d'
