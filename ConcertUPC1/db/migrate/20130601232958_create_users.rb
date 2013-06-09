07f7f7e320585088f5818fc1819305b85b4d195a
Augusto Bravo <bravoaugusto300@gmail.com>
2013-06-08 22:40:34 -0500

    login

--- ConcertUPC1/db/migrate/20130601232958_create_users.rb
+++ ConcertUPC1/db/migrate/20130601232958_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :username,         :null => false  # if you use another field as a username, for example email, you can safely remove this field.
       t.integer :typeuser
       t.string :name
       t.string :lastname
@@ -9,10 +10,8 @@ class CreateUsers < ActiveRecord::Migration
       t.integer :typedocument
       t.string :document
       t.string :email
-      t.string :password
-      t.string :email_confirmation
-      t.string :password_confirmation
-
+      t.string :crypted_password, :default => nil
+      t.string :salt,             :default => nil
       t.timestamps
     end
   end
